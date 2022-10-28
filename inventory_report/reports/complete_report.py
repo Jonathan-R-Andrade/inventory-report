7c2a5e085bc78c026e8ab25042575196256d6e0b
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-10-28 18:49:08 -0300

    feat: adiciona CompleteReport

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products):
+        report = super().generate(products)
+        companies = [product["nome_da_empresa"] for product in products]
+        products_by_companies = Counter(companies).items()
+        list_of_products_by_companies = [
+            f"- {company}: {total_product}\n"
+            for company, total_product in products_by_companies
+        ]
+        report_of_products_by_companies = "".join(
+            list_of_products_by_companies
+        )
+        return (
+            f"{report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{report_of_products_by_companies}"
+        )
