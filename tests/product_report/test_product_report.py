076d682b3a785026027df5214bb61eab4cac5bef
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-10-28 15:53:05 -0300

    test: testa relatório de produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pf = ProductFactory()
+    product = Product(
+        pf.id,
+        pf.nome_do_produto,
+        pf.nome_da_empresa,
+        pf.data_de_fabricacao,
+        pf.data_de_validade,
+        pf.numero_de_serie,
+        pf.instrucoes_de_armazenamento,
+    )
+
+    expected_phrase = (
+        f"O produto {pf.nome_do_produto}"
+        f" fabricado em {pf.data_de_fabricacao}"
+        f" por {pf.nome_da_empresa} com validade"
+        f" até {pf.data_de_validade}"
+        f" precisa ser armazenado {pf.instrucoes_de_armazenamento}."
+    )
+
+    assert str(product) == expected_phrase
