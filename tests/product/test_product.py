81f149aa073957d01e725eac320ef23f6e04af9e
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-10-28 15:31:18 -0300

    test: testa test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    pf = ProductFactory()
+    product = Product(
+        pf.id,
+        pf.nome_do_produto,
+        pf.nome_da_empresa,
+        pf.data_de_fabricacao,
+        pf.data_de_validade,
+        pf.numero_de_serie,
+        pf.instrucoes_de_armazenamento,
+    )
+
+    assert product.id == pf.id
+    assert product.nome_do_produto == pf.nome_do_produto
+    assert product.nome_da_empresa == pf.nome_da_empresa
+    assert product.data_de_fabricacao == pf.data_de_fabricacao
+    assert product.data_de_validade == pf.data_de_validade
+    assert product.numero_de_serie == pf.numero_de_serie
+    assert (
+        product.instrucoes_de_armazenamento == pf.instrucoes_de_armazenamento
+    )
