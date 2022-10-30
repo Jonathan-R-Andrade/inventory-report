d2bd94f0f76ab5c9fe75627e23611a8fe01f6445
Jonathan Reis de Andrade <jonathanreisdeandrade@gmail.com>
2022-10-30 18:25:55 -0300

    feat: preenche a função main de inventory_report

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,15 @@
+import sys
+from inventory_report.importer.importers import importers
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        print("Verifique os argumentos", file=sys.stderr)
+        return 1
+    file_type = sys.argv[1].split(".")[-1].lower()
+    importer = importers[file_type]
+    inventory = InventoryRefactor(importer)
+    report = inventory.import_data(sys.argv[1], sys.argv[2])
+    print(report, end="")
+    return 0
