e7f23b8f2349afc7eee6c7c67a5436d63c553a58
Jonathan R. Andrade <jonathanreisdeandrade@gmail.com>
2023-07-15 22:11:26 -0300

    refactor: colore todo o relatório completo

--- inventory_report/reports/colored_report.py
+++ inventory_report/reports/colored_report.py
@@ -5,6 +5,39 @@ class ColoredReport:
     def __init__(self, report_type):
         self.report_type = report_type
 
+    def __colorize_dates(self, report, color_code):
+        dates = re.findall(r"(\d+-\d+-\d+)", report)
+
+        for date in dates:
+            report = report.replace(
+                date,
+                f"\033[{color_code}m{date}\033[0m",
+            )
+
+        return report
+
+    def __colorize_companies(self, report, color_code):
+        companies = re.findall(r"(- .+:)", report)
+
+        for company in companies:
+            report = report.replace(
+                company,
+                f"- \033[{color_code}m{company[2:-1]}\033[0m:",
+            )
+
+        return report
+
+    def __colorize_total_stocked_products(self, report, color_code):
+        total_stocked_products = re.findall(r"(: \d+)", report)
+
+        for stocked_product in total_stocked_products:
+            report = report.replace(
+                stocked_product,
+                f": \033[{color_code}m{stocked_product[2:]}\033[0m",
+            )
+
+        return report
+
     def generate(self, products_list):
         report = self.report_type.generate(products_list)
         index_start = report.find("mais produtos:") + 15
@@ -24,6 +57,7 @@ class ColoredReport:
             "Data de fabricação mais antiga:",
             "Data de validade mais próxima:",
             "Empresa com mais produtos:",
+            "Produtos estocados por empresa:",
         ]
 
         for phrase in green_phrases:
@@ -32,12 +66,8 @@ class ColoredReport:
                 f"\033[32m{phrase}\033[0m",
             )
 
-        report_dates = re.findall(r"(\d+-\d+-\d+)", report)
-
-        for date in report_dates:
-            report = report.replace(
-                date,
-                f"\033[36m{date}\033[0m",
-            )
+        report = self.__colorize_dates(report, 36)
+        report = self.__colorize_companies(report, 31)
+        report = self.__colorize_total_stocked_products(report, 34)
 
         return report
